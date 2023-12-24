a0e43d2dc645c41d4a223365f219ee096d49eb13
Nikesh Adhikari <nikeshad254@gmail.com>
2023-12-24 16:50:47 +0545

    basic user route and controller for register setup

--- src/app.js
+++ src/app.js
@@ -23,4 +23,10 @@ app.use(express.static("public"));
 // to parse cookies
 app.use(cookieParser());
 
+// Routes Import
+import userRouter from "./routes/user.routes.js";
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter);
+
 export default app;
