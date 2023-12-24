af917f14757040d34bf68a738405593e8636c9e3
Nikesh Adhikari <nikeshad254@gmail.com>
2023-12-24 10:09:42 +0545

    apierror, apiresponse and async handler created

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// to limit how much json data can be sent to the server
+app.use(express.json({ limit: "16kb" }));
+
+// to parse url encoded data | extended: true means that we can send nested objects
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// to serve static files
+app.use(express.static("public"));
+
+// to parse cookies
+app.use(cookieParser());
+
+export default app;
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+/**
+ * @description - A utility function to handle async/await errors
+ * @param {Function} requestHandler - The function to be executed
+ */
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+/*      using try and catch
+
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+*/
+
+export { asyncHandler };
